eaf16489cf95219414881141b855b7c93dc56b54
Efe Akaröz <efeakaroz13@gmail.com>
2021-08-22 22:02:25 +0300

    Initial Commit

---
+++ Civic Sayar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Civic Sayar
+//
+//  Created by Efe Akaröz on 22.08.2021.
+//  Copyright © 2021 kentel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
